79db382ec821c15da45dd84c3123842c3286f138
Syed Kounain Abbas Rizvi <abbasmashaddy72@gmail.com>
2024-01-24 16:48:12 +0530

    Renamimg of alis and setting up middleware for 503

--- src/middlewares/maintenanceMiddleware.tsx
+++ src/middlewares/maintenanceMiddleware.tsx
@@ -5,13 +5,12 @@ export function MaintenanceMiddleware({ children }: PropsWithChildren) {
   const [isServiceUnavailable, setIsServiceUnavailable] = useState(false);
 
   useEffect(() => {
-    // Simulate a maintenance scenario (replace this with your actual logic)
-    const timeoutId = setTimeout(() => {
-      setIsServiceUnavailable(true);
-    }, 10000); // 10 seconds for demonstration, adjust as needed
+    const isMaintenanceMode = import.meta.env.VITE_MAINTENANCE === 'true';
 
-    // Cleanup the timeout to avoid memory leaks
-    return () => clearTimeout(timeoutId);
+    // If in maintenance mode, immediately set service as unavailable
+    if (isMaintenanceMode) {
+      setIsServiceUnavailable(true);
+    }
   }, []);
 
   if (isServiceUnavailable) {
